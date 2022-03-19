ce32120f52e2cf354e7fb9b090db5bc34c512b62
electro898 <101351317+electro898@users.noreply.github.com>
2022-03-19 15:01:47 +0100

    Update analytics.js

--- public/scripts/analytics.js
+++ public/scripts/analytics.js
@@ -1,7 +1,10 @@
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+var imported = document.createElement('script');
+imported.src = 'https://www.googletagmanager.com/gtag/js?id=UA-110501913-1';
+document.head.appendChild(imported);
 
-  gtag('config', 'UA-110501913-1');
-</script>
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-110501913-1');
